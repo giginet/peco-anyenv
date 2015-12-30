8114d01a7bd8198067db59debfccaa28072a6da0
giginet <giginet.net@gmail.com>
2015-12-30 13:26:32 +0900

    Refactor peco-anyenv

--- peco-anyenv.sh
+++ peco-anyenv.sh
@@ -1,46 +1,48 @@
 #/usr/bin/env sh
 function _switch_env() {
-    if [[ $fooenv == "" || $language_name == "" ]]; then
-      exit
-    fi
+  if [[ $fooenv == "" || $language_name == "" ]]; then
+    return 0
+  fi
 
-    venvs=`$fooenv versions | peco`
-    venv0=`echo $venvs | cut -d' ' -f1`
-    venv1=`echo $venvs | cut -d' ' -f2`
-    if [[ $venv0 == "*" ]]; then
-      venv_name=$venv1
-    else
-      venv_name=`echo $venvs| awk '{ print $1 }'`
-    fi
+  versions=`$fooenv versions | peco`
+  if [[ $versions == "" ]]; then
+    return 0
+  fi
 
-    mode=local
-    if [[ $1 == "--global" ]]; then
-      mode=global
-    fi
-    echo "Set ${mode} ${language_name} version to ${venv_name}"
-    $fooenv $mode $venv_name
+  if [[ $versions == \** ]]; then
+    version_name=`echo $versions | cut -d' ' -f1`
+  else
+    version_name=`echo $versions | awk '{ print $1 }'`
+  fi
+
+  mode=local
+  if [[ $1 == "--global" ]]; then
+    mode=global
+  fi
+  echo "Set ${mode} ${language_name} version to ${version_name}"
+  $fooenv $mode $version_name
 }
 
 function ppyenv() {
-    language_name='Python'
-    fooenv='pyenv'
-    _switch_env
+  language_name='Python'
+  fooenv='pyenv'
+  _switch_env
 }
 
 function prbenv() {
-    language_name='Ruby'
-    fooenv='rbenv'
-    _switch_env
+  language_name='Ruby'
+  fooenv='rbenv'
+  _switch_env
 }
 
 function pplenv() {
-    language_name='Perl'
-    fooenv='plenv'
-    _switch_env
+  language_name='Perl'
+  fooenv='plenv'
+  _switch_env
 }
 
 function pndenv() {
-    language_name='Node'
-    fooenv='ndenv'
-    _switch_env
+  language_name='Node'
+  fooenv='ndenv'
+  _switch_env
 }
